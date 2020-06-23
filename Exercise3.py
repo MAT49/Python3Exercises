cb1cdc3138b2838ee8b7a9c4f1d3367c31e13cf9
Ha Hwang <spyderthing@gmail.com>
2020-06-23 15:53:33 -0400

    Changed output to float data type

--- Exercise3.py
+++ Exercise3.py
@@ -9,7 +9,7 @@ Created on Tue Jun 23 15:27:19 2020
 print("I will now count my chickens")
 
 print("Hens", 25 + 30 /6)
-print("Roosters", 100 - 25 * 3 % 4)
+print("Roosters", 100. - 25 * 3 % 4)
 
 print("Now I will count the eggs")
 
