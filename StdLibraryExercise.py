97757438d6fabe3ff4d4c83b16b4e3f840244d8a
Ha Hwang <spyderthing@gmail.com>
2020-06-23 17:34:55 -0400

    Created Exercise4.py and StdLibraryExercise.py

--- StdLibraryExercise.py
+++ StdLibraryExercise.py
@@ -9,13 +9,32 @@ Created on Tue Jun 23 17:04:17 2020
 from os import getcwd, environ
 import sys
 import os
+import datetime
+import time
+import html
 
 
-current = getcwd()
-# print(current)
-print(sys.platform)
+# current = getcwd()
+# # print(current)
+# print(sys.platform)
 
-print(os.environ)
-print(os.getenv('HOME'))
-
+# print(os.environ)
 # print(os.getenv('HOME'))
+
+# # print(os.getenv('HOME'))
+
+# print(datetime.date.today())
+# print(datetime.date.today().day)
+# print(datetime.date.today().month)
+# print(datetime.date.today().year)
+
+# str = datetime.date.isoformat(datetime.date.today())
+# print(str)
+
+# print(time.strftime("%H:%M:%S"))
+
+# print(time.strftime("%A %p"))
+
+print(html.escape("This HTML fragment contains a <script>script</script> tag."))
+
+print(html.unescape("I &hearts; Python's &lt;standard library&gt;."))
